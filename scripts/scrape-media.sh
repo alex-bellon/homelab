559aae9bf67d91b0b9a369f85840df04f8204890
alex-bellon <alexrbellon@gmail.com>
2021-06-07 22:58:47 -0500

    Consolidated scripts

--- scripts/scrape-media.sh
+++ scripts/scrape-media.sh
@@ -1,4 +1,7 @@
 DIR=/home/alex/GitHub/brain
+
+# Video 
+
 REGEX='https://www.youtube.com/watch.*|https://youtu.be.*'
 
 # I know this is so gross but I couldn't figure out how else to do this :'(
@@ -6,4 +9,14 @@ GREP=$(egrep -r $REGEX $DIR --include \*.md -o | sed 's/^[^:]*:/:/' | sed 's/).*
 
 for i in $GREP; do
     youtube-dl -o '/media/alex/Media/Backup/brain/youtube/%(title)s' "$i"
+done
+
+# Reddit
+
+REGEX='(http://|https://)?(www.)?reddit.com/r/.*?/.*?/'
+
+GREP=$(egrep -r $REGEX $DIR --include \*.md -o | sed 's/^[^:]*:/:/' | sed 's/).*//' | cut -c 2-)
+
+for i in $GREP; do
+    python3 -m bdfr clone /media/alex/Media/Backup/brain/reddit/ -l "$i"
 done
