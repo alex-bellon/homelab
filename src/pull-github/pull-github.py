ceafb7d33ce6d9e939b097fd7c1aee863ee7be9b
alex-bellon <alexrbellon@gmail.com>
2022-08-23 23:07:09 -0700

    Added pagination to github pull

--- src/pull-github/pull-github.py
+++ src/pull-github/pull-github.py
@@ -6,21 +6,25 @@ load_dotenv('.env')
 GITHUB_KEY = os.getenv('GITHUB_KEY')
 
 headers = {"Authorization": "token " + GITHUB_KEY}
-url = "https://api.github.com/user/repos?per_page=200&type=owner"
 
-response = requests.get(url, headers=headers)
-repos = response.json()
+for i in range(1, 100):
+    url = "https://api.github.com/user/repos?per_page=100&type=owner&page=" + str(i) 
+    
+    response = requests.get(url, headers=headers)
+    repos = response.json()
+    
+    if not repos: break
+    
+    for repo in repos:
+        name = repo["name"]
+        directory = "/home/alex/GitHub/" + name
+        source = repo["ssh_url"]
 
-for repo in repos:
-    name = repo["name"]
-    directory = "/home/alex/GitHub/" + name
-    source = repo["ssh_url"]
-
-    if os.path.exists(directory):
-        print("Pulling " + directory)
-        os.chdir(directory)
-        os.system("git pull")
-    else:
-        print("Creating " + directory)
-        os.chdir("/home/alex/GitHub/")
-        os.system("git clone " + source)
+        if os.path.exists(directory):
+            print("Pulling " + directory)
+            os.chdir(directory)
+            os.system("git pull")
+        else:
+            print("Creating " + directory)
+            os.chdir("/home/alex/GitHub/")
+            os.system("git clone " + source)
